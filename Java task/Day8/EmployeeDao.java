8cc2c15eb7fa2c4b8a73829483f470aa17a19592
SelvakumarRavi <134347736+SelvakumarRavi@users.noreply.github.com>
2023-06-13 21:05:02 +0530

    EmployeeDao.java

--- Java task/Day8/EmployeeDao.java
+++ Java task/Day8/EmployeeDao.java
@@ -10,69 +10,71 @@ import com.dal.model.Employee;
 import com.dal.util.MyDbConnection;
 
 public class EmployeeDao {
-
+	
 	Connection con;
 	Statement stmt;
 	PreparedStatement ps;
 	ResultSet rs;
-
+	
 	public void insertEmployee(Employee emp) {
-
-		con = MyDbConnection.getMyConnection();
+		
+		con =MyDbConnection.getMyConnection();
 		try {
-			ps = con.prepareStatement("insert into dalemp values(?,?)");
-			ps.setString(1, emp.getEname());
-			ps.setInt(2, emp.getEmpno());
-
-			int noofrows = ps.executeUpdate();
+			ps =con.prepareStatement("insert into DEDEMP values(?,?)");
+			ps.setInt(1, emp.getEmpno());
+			ps.setString(2, emp.getEname());
+			int noofrows =ps.executeUpdate();
 			System.out.println(noofrows + " got inserted into DB successfully!");
 		} catch (SQLException e) {
-
+		
 			e.printStackTrace();
 		}
-
+		
 	}
-
+	
 	public void showEmployee() {
-		con = MyDbConnection.getMyConnection();
+		con =MyDbConnection.getMyConnection();
 		try {
-			stmt = con.createStatement();
-			rs = stmt.executeQuery("select * from dalemp");
-			while (rs.next()) {
-				System.out.println(rs.getInt(2) + " -- " + rs.getString(1));
+			stmt =con.createStatement();
+			rs =stmt.executeQuery("select * from dedemp");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1) + " -- " + rs.getString(2));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-	}
-
-	public void updateEmployee(Employee emp) {
-		con = MyDbConnection.getMyConnection();
-		try {
-
-			ps = con.prepareStatement("update dalemp set empname = ? where empno = ?");
-			ps.setString(1, emp.getEname());
-			ps.setInt(2, emp.getEmpno());
-
-			int noofrows = ps.executeUpdate();
-			System.out.println(noofrows + " employee(s) updated successfully!");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		
+		
 	}
 	
-	public void deleteEmployee(int empno) {
-		con = MyDbConnection.getMyConnection();
+	public void UpdateEmployee(int eno,String ename) {
+		con =MyDbConnection.getMyConnection();
 		try {
-			ps = con.prepareStatement("delete from dalemp where EMPNO = ?");
-			ps.setInt(1, empno);
-			
-			int noofrows = ps.executeUpdate();
-			System.out.println(noofrows + " employee(s) deleted successfully!");
+			stmt =con.createStatement();
+			stmt.executeUpdate("UPDATE dedemp SET ename = '"+ename+"' WHERE empno = "+eno);
+			System.out.println(eno + "employee got updated in DB successfully!");
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
+	
+	public void DeleteEmployee(int eno) {
+		con =MyDbConnection.getMyConnection();
+		try {
+			stmt =con.createStatement();
+			stmt.executeUpdate("delete from dedemp where empno="+eno);
+			System.out.println(eno + "employee Deleted from DB successfully!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
 }
